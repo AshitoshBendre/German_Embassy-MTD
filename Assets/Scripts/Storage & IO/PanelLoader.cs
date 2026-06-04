c33e44ccade095efa54be51ddc732b7a4619006f
Ashitosh Bendre (Office) <ashitoshbendre.work@gmail.com>
2026-06-04 17:57:00 +0530

    Panels Updates

---
+++ Assets/Scripts/Storage & IO/PanelLoader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelLoader : MonoBehaviour, IStorageLoader
+{
+    public void LoadAsync(string path)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Storage & IO/ProjectLoader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectLoader : MonoBehaviour, IStorageLoader
+{
+    public void LoadAsync(string path)
+    {
+        throw new System.NotImplementedException();
+    }
+}
